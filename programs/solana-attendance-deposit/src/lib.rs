88b30f9c3cac216523dad66b6a34192997d5d83f
Seth <hullogh@gmail.com>
2024-04-30 02:40:15 +0800

    feat: add initialise and register course

--- programs/solana-attendance-deposit/src/lib.rs
+++ programs/solana-attendance-deposit/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token::{self, TokenAccount, Token, Transfer, Mint};
 
 declare_id!("G8PAMHAVZVAzcsKAXVtuJ69msx9tB1tNYKRPoSCtZ54G");
 
@@ -6,10 +8,134 @@ declare_id!("G8PAMHAVZVAzcsKAXVtuJ69msx9tB1tNYKRPoSCtZ54G");
 pub mod solana_attendance_deposit {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn create_course(ctx: Context<NewCourse>, name: String, deposit: u64, lock_until: u64) -> Result<()> {
+        let course = &mut ctx.accounts.course;
+        course.new(name, ctx.accounts.manager.key(), deposit, lock_until)
+    }
+
+    pub fn register(ctx: Context<Registration>) -> Result<()> {
+        let course = &mut ctx.accounts.course;
+        let student = &ctx.accounts.student;
+
+        let student_balance = ctx.accounts.student_usdc.amount;
+        if student_balance < course.deposit {
+            return Err(ErrorCode::InsufficientUsdcDeposit.into());
+        }
+
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.student_usdc.to_account_info(),
+            to: ctx.accounts.course_usdc.to_account_info(),
+            authority: ctx.accounts.student.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        token::transfer(cpi_ctx, course.deposit)?;
+
+        course.register(student.key())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+#[instruction(name: String, deposit: u64, lock_until: u64)]
+pub struct NewCourse<'info> {
+    #[account(
+    init,
+    payer = manager,
+    space = 8 + std::mem::size_of::< Course > (),
+    seeds = [name.as_bytes()],
+    bump,
+    )]
+    pub course: Account<'info, Course>,
+    #[account(mut)]
+    pub manager: Signer<'info>,
+    #[account(
+    init_if_needed,
+    payer = manager,
+    associated_token::mint = usdc_mint,
+    associated_token::authority = course,
+    )]
+    pub course_usdc: Account<'info, TokenAccount>,
+    pub usdc_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Registration<'info> {
+    #[account(
+    mut,
+    seeds = [course.name.as_bytes()],
+    bump,
+    realloc = course.to_account_info().data_len() + std::mem::size_of::< Pubkey > (),
+    realloc::payer = student,
+    realloc::zero = false,
+    )]
+    pub course: Account<'info, Course>,
+    #[account(mut)]
+    pub student: Signer<'info>,
+    #[account(
+    init_if_needed,
+    payer = student,
+    associated_token::mint = usdc_mint,
+    associated_token::authority = student,
+    )]
+    pub student_usdc: Account<'info, TokenAccount>,
+    #[account(
+    init_if_needed,
+    payer = student,
+    associated_token::mint = usdc_mint,
+    associated_token::authority = course,
+    )]
+    pub course_usdc: Account<'info, TokenAccount>,
+    pub usdc_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Course {
+    pub name: String,
+    pub manager: Pubkey,
+    pub students: Vec<Pubkey>,
+    pub deposit: u64,
+    pub lock_until: u64,
+}
+
+impl Course {
+    pub const MAXIMUM_SIZE: usize = 32 + 32 + 32 + 8 + 8;
+
+    pub fn new(&mut self, name: String, manager: Pubkey, deposit: u64, lock_until: u64) -> Result<()> {
+        self.name = name;
+        self.manager = manager;
+        self.deposit = deposit;
+        self.lock_until = lock_until;
+
+        Ok(())
+    }
+
+    pub fn register(&mut self, student: Pubkey) -> Result<()> {
+        if self.students.contains(&student) {
+            return Err(ErrorCode::StudentAlreadyEnrolled.into());
+        }
+
+        self.students.push(student.key());
+
+        Ok(())
+    }
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Student is not enrolled in the course")]
+    StudentNotEnrolled,
+    #[msg("Student already enrolled in the course")]
+    StudentAlreadyEnrolled,
+    #[msg("Insufficient USDCC balance for deposit")]
+    InsufficientUsdcDeposit,
+}
--- tests/solana-attendance-deposit.ts
+++ tests/solana-attendance-deposit.ts
@@ -1,16 +1,400 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import {
+  getOrCreateAssociatedTokenAccount,
+  createMint,
+  mintTo,
+  Account,
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  getAssociatedTokenAddressSync,
+  getAccount,
+} from "@solana/spl-token";
 import { SolanaAttendanceDeposit } from "../target/types/solana_attendance_deposit";
+import { expect } from "chai";
 
 describe("solana-attendance-deposit", () => {
+  const provider = anchor.AnchorProvider.env();
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.SolanaAttendanceDeposit as Program<SolanaAttendanceDeposit>;
+  const program = anchor.workspace
+    .SolanaAttendanceDeposit as Program<SolanaAttendanceDeposit>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const LAMPORTS_PER_SOL = 1000000000;
+
+  const splMintAuthority = anchor.web3.Keypair.generate();
+
+  const courseManager = anchor.web3.Keypair.generate();
+  const student1 = anchor.web3.Keypair.generate();
+  const student2 = anchor.web3.Keypair.generate();
+
+  let student1UsdcAta: Account;
+  let student2UsdcAta: Account;
+  let usdcMint: anchor.web3.PublicKey;
+
+  before(async () => {
+    const connection = provider.connection;
+    const latestBlockHash = await provider.connection.getLatestBlockhash();
+    await provider.connection.confirmTransaction(
+      {
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: await connection.requestAirdrop(
+          courseManager.publicKey,
+          LAMPORTS_PER_SOL
+        ),
+      },
+      "confirmed"
+    );
+    await provider.connection.confirmTransaction(
+      {
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: await connection.requestAirdrop(
+          student1.publicKey,
+          LAMPORTS_PER_SOL
+        ),
+      },
+      "confirmed"
+    );
+    await provider.connection.confirmTransaction(
+      {
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: await connection.requestAirdrop(
+          student2.publicKey,
+          LAMPORTS_PER_SOL
+        ),
+      },
+      "confirmed"
+    );
+
+    await provider.connection.confirmTransaction(
+      {
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: await connection.requestAirdrop(
+          splMintAuthority.publicKey,
+          LAMPORTS_PER_SOL
+        ),
+      },
+      "confirmed"
+    );
+
+    usdcMint = await createMint(
+      provider.connection,
+      splMintAuthority,
+      splMintAuthority.publicKey,
+      null,
+      6
+    );
+
+    student1UsdcAta = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      splMintAuthority,
+      usdcMint,
+      student1.publicKey
+    );
+
+    student2UsdcAta = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      splMintAuthority,
+      usdcMint,
+      student2.publicKey
+    );
+
+    await mintTo(
+      provider.connection,
+      splMintAuthority,
+      usdcMint,
+      student1UsdcAta.address,
+      splMintAuthority.publicKey,
+      1000 * 10 ** 6
+    );
+
+    await mintTo(
+      provider.connection,
+      splMintAuthority,
+      usdcMint,
+      student2UsdcAta.address,
+      splMintAuthority.publicKey,
+      1000 * 10 ** 6
+    );
+  });
+
+  describe("Initialise a new course", () => {
+    const courseTitle = "Learn Solana";
+    let coursePda: anchor.web3.PublicKey;
+
+    before(async () => {
+      [coursePda] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from(anchor.utils.bytes.utf8.encode(courseTitle))],
+        program.programId
+      );
+    });
+
+    it("should initialise a new course", async () => {
+      const timestamp = Math.floor(Date.now() / 1000);
+      await program.methods
+        .createCourse(
+          courseTitle,
+          new anchor.BN(250),
+          new anchor.BN(timestamp + 60 * 60 * 24 * 7)
+        )
+        .accounts({
+          course: coursePda,
+          manager: courseManager.publicKey,
+          usdcMint: usdcMint,
+          courseUsdc: getAssociatedTokenAddressSync(usdcMint, coursePda, true),
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([courseManager])
+        .rpc();
+
+      const courseData = await program.account.course.fetch(coursePda);
+
+      expect(courseData.name).to.equal(courseTitle);
+    });
+  });
+
+  describe.only("Student Registration", () => {
+    const courseTitle = "Course Title - Registration";
+    let coursePda: anchor.web3.PublicKey;
+
+    let currentTimestamp: number;
+    let lockUntilTimestamp: number;
+
+    before(async () => {
+      [coursePda] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from(anchor.utils.bytes.utf8.encode(courseTitle))],
+        program.programId
+      );
+
+      currentTimestamp = Math.floor(Date.now() / 1000);
+      lockUntilTimestamp = currentTimestamp + 60 * 60 * 24 * 7;
+
+      await program.methods
+        .createCourse(
+          courseTitle,
+          new anchor.BN(250 * 10 ** 6),
+          new anchor.BN(lockUntilTimestamp)
+        )
+        .accounts({
+          course: coursePda,
+          manager: courseManager.publicKey,
+          usdcMint: usdcMint,
+          courseUsdc: getAssociatedTokenAddressSync(usdcMint, coursePda, true),
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([courseManager])
+        .rpc()
+        .catch(console.error);
+    });
+
+    describe("When registering student1", () => {
+      let student1UsdcBalanceBefore: bigint;
+
+      before(async () => {
+        student1UsdcBalanceBefore = (
+          await getAccount(provider.connection, student1UsdcAta.address)
+        ).amount;
+        await program.methods
+          .register()
+          .accounts({
+            course: coursePda,
+            student: student1.publicKey,
+            studentUsdc: getAssociatedTokenAddressSync(
+              usdcMint,
+              student1.publicKey
+            ),
+            usdcMint: usdcMint,
+            courseUsdc: getAssociatedTokenAddressSync(
+              usdcMint,
+              coursePda,
+              true
+            ),
+            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            systemProgram: anchor.web3.SystemProgram.programId,
+          })
+          .signers([student1])
+          .rpc()
+          .catch(console.error);
+      });
+
+      it("should register student1 with the course", async () => {
+        const courseData = await program.account.course.fetch(coursePda);
+
+        expect(courseData.students[0].toBase58()).to.equal(
+          student1.publicKey.toBase58()
+        );
+        expect(courseData.students.length).to.equal(1);
+      });
+
+      it("should receive deposit from student1", async () => {
+        const student1UsdcBalanceAfter = (
+          await getAccount(provider.connection, student1UsdcAta.address)
+        ).amount;
+        const courseUsdcBalance = (
+          await getAccount(
+            provider.connection,
+            getAssociatedTokenAddressSync(usdcMint, coursePda, true)
+          )
+        ).amount;
+
+        expect(student1UsdcBalanceBefore - student1UsdcBalanceAfter).to.equal(
+          BigInt(250 * 10 ** 6)
+        );
+        expect(courseUsdcBalance).to.equal(BigInt(250 * 10 ** 6));
+      });
+
+      it("should not allow student1 to register again with the course", async () => {
+        try {
+          await program.methods
+            .register()
+            .accounts({
+              course: coursePda,
+              student: student1.publicKey,
+              studentUsdc: getAssociatedTokenAddressSync(
+                usdcMint,
+                student1.publicKey
+              ),
+              usdcMint: usdcMint,
+              courseUsdc: getAssociatedTokenAddressSync(
+                usdcMint,
+                coursePda,
+                true
+              ),
+              associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+              tokenProgram: TOKEN_PROGRAM_ID,
+              systemProgram: anchor.web3.SystemProgram.programId,
+            })
+            .signers([student1])
+            .rpc();
+
+          expect(true, "Transaction did not revert as expected").to.be.false;
+        } catch (e: unknown) {
+          expect((e as anchor.AnchorError).message).to.contain(
+            "StudentAlreadyEnrolled"
+          );
+        }
+      });
+    });
+
+    describe("When registering student2", () => {
+      let student2UsdcBalanceBefore: bigint;
+
+      before(async () => {
+        student2UsdcBalanceBefore = (
+          await getAccount(provider.connection, student2UsdcAta.address)
+        ).amount;
+
+        await program.methods
+          .register()
+          .accounts({
+            course: coursePda,
+            student: student2.publicKey,
+            studentUsdc: getAssociatedTokenAddressSync(
+              usdcMint,
+              student2.publicKey
+            ),
+            usdcMint: usdcMint,
+            courseUsdc: getAssociatedTokenAddressSync(
+              usdcMint,
+              coursePda,
+              true
+            ),
+            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            systemProgram: anchor.web3.SystemProgram.programId,
+          })
+          .signers([student2])
+          .rpc()
+          .catch(console.error);
+      });
+
+      it("should register student2 with the course", async () => {
+        const courseData = await program.account.course.fetch(coursePda);
+
+        expect(courseData.students[1].toBase58()).to.equal(
+          student2.publicKey.toBase58()
+        );
+        expect(courseData.students.length).to.equal(2);
+      });
+
+      it("should receive deposit from student2", async () => {
+        const student2UsdcBalanceAfter = (
+          await getAccount(provider.connection, student2UsdcAta.address)
+        ).amount;
+        const courseUsdcBalance = (
+          await getAccount(
+            provider.connection,
+            getAssociatedTokenAddressSync(usdcMint, coursePda, true)
+          )
+        ).amount;
+
+        expect(student2UsdcBalanceBefore - student2UsdcBalanceAfter).to.equal(
+          BigInt(250 * 10 ** 6)
+        );
+        expect(courseUsdcBalance).to.equal(BigInt(500 * 10 ** 6));
+      });
+    });
+
+    describe("When registering a student with insufficient deposit", () => {
+      let student3: anchor.web3.Keypair;
+
+      before(async () => {
+        student3 = anchor.web3.Keypair.generate();
+        const latestBlockHash = await provider.connection.getLatestBlockhash();
+        await provider.connection.confirmTransaction(
+          {
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: await provider.connection.requestAirdrop(
+              student3.publicKey,
+              LAMPORTS_PER_SOL
+            ),
+          },
+          "confirmed"
+        );
+      });
+
+      it("should fail with InsufficientUsdcDeposit", async () => {
+        try {
+          await program.methods
+            .register()
+            .accounts({
+              course: coursePda,
+              student: student3.publicKey,
+              studentUsdc: getAssociatedTokenAddressSync(
+                usdcMint,
+                student3.publicKey
+              ),
+              usdcMint: usdcMint,
+              courseUsdc: getAssociatedTokenAddressSync(
+                usdcMint,
+                coursePda,
+                true
+              ),
+              associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+              tokenProgram: TOKEN_PROGRAM_ID,
+              systemProgram: anchor.web3.SystemProgram.programId,
+            })
+            .signers([student3])
+            .rpc();
+
+          expect(true, "Transaction did not revert as expected").to.be.false;
+        } catch (e: unknown) {
+          expect((e as anchor.AnchorError).message).to.contain(
+            "InsufficientUsdcDeposit"
+          );
+        }
+      });
+    });
   });
 });
